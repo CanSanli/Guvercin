a1df852c0699c4e6a0be71b070d150edc8c0383a
Bilge-Han <bilgeacar095@gmail.com>
2024-07-07 17:49:01 +0300

    profildüzenleok

--- Guvercin.Web/Controllers/AccController.cs
+++ Guvercin.Web/Controllers/AccController.cs
@@ -44,5 +44,107 @@ namespace GuvercinApp.Controllers
             FormsAuthentication.SignOut();
             return RedirectToAction("Login");
         }
+
+        [HttpGet]
+        public ActionResult Profile()
+        {
+            if (Session["KullaniciId"] == null)
+            {
+                return RedirectToAction("Login", "Acc");
+            }
+            // Kullanıcı Id'sini Session'dan al
+            int kullaniciId = (int)Session["KullaniciId"];
+
+            // Kullanıcıyı ve ilişkili bilgileri getir
+            var kullanici = _context.Kullanicilar.FirstOrDefault(k => k.KullaniciId == kullaniciId);
+            if (kullanici == null)
+            {
+                return HttpNotFound("Kullanıcı bulunamadı.");
+            }
+
+            if (kullanici.KullaniciTipi == "bireysel")
+            {
+                var bireyselKullanici = _context.BireyselKullanicilar.FirstOrDefault(b => b.KullaniciId == kullaniciId);
+                if (bireyselKullanici == null)
+                {
+                    return HttpNotFound("Bireysel kullanıcı bulunamadı.");
+                }
+
+                // ViewModel oluştur
+                var model = new ProfileViewModel
+                {
+                    Kullanici = kullanici,
+                    BireyselKullanici = bireyselKullanici
+                };
+
+                return View(model);
+            }
+            else if (kullanici.KullaniciTipi == "kurumsal")
+            {
+                var kurumsalKullanici = _context.KurumsalKullanicilar.FirstOrDefault(k => k.KullaniciId == kullaniciId);
+                if (kurumsalKullanici == null)
+                {
+                    return HttpNotFound("Kurumsal kullanıcı bulunamadı.");
+                }
+
+                // ViewModel oluştur
+                var model = new ProfileViewModel
+                {
+                    Kullanici = kullanici,
+                    KurumsalKullanici = kurumsalKullanici
+                };
+
+                return View(model);
+            }
+
+            return HttpNotFound("Kullanıcı tipi belirlenemedi.");
+        }
+
+
+        [HttpPost]
+        public ActionResult SaveProfile(ProfileViewModel model)
+        {
+            // Kullanıcıyı güncelle
+            var kullanici = _context.Kullanicilar.FirstOrDefault(k => k.KullaniciId == model.Kullanici.KullaniciId);
+            if (kullanici == null)
+            {
+                return HttpNotFound("Kullanıcı bulunamadı.");
+            }
+
+            kullanici.Email = model.Kullanici.Email;
+            kullanici.Telefon = model.Kullanici.Telefon;
+            kullanici.Adres = model.Kullanici.Adres;
+
+            // Bireysel kullanıcı ise bireysel bilgileri de güncelle
+            if (model.Kullanici.KullaniciTipi == "bireysel")
+            {
+                var bireyselKullanici = _context.BireyselKullanicilar.FirstOrDefault(b => b.KullaniciId == model.Kullanici.KullaniciId);
+                if (bireyselKullanici == null)
+                {
+                    return HttpNotFound("Bireysel kullanıcı bulunamadı.");
+                }
+
+                bireyselKullanici.Ad = model.BireyselKullanici.Ad;
+                bireyselKullanici.Soyad = model.BireyselKullanici.Soyad;
+                bireyselKullanici.DogumTarihi = model.BireyselKullanici.DogumTarihi;
+            }
+            // Kurumsal kullanıcı ise kurumsal bilgileri de güncelle
+            else if (model.Kullanici.KullaniciTipi == "kurumsal")
+            {
+                var kurumsalKullanici = _context.KurumsalKullanicilar.FirstOrDefault(k => k.KullaniciId == model.Kullanici.KullaniciId);
+                if (kurumsalKullanici == null)
+                {
+                    return HttpNotFound("Kurumsal kullanıcı bulunamadı.");
+                }
+
+                kurumsalKullanici.KurumAdi = model.KurumsalKullanici.KurumAdi;
+                kurumsalKullanici.VergiNo = model.KurumsalKullanici.VergiNo;
+                kurumsalKullanici.KurulusTarihi = model.KurumsalKullanici.KurulusTarihi;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Profile");
+        }
     }
 }
---
+++ Guvercin.Web/Models/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuvercinApp.Models
+{
+    public class ProfileViewModel
+    {
+        public Kullanici Kullanici { get; set; }
+        public BireyselKullanici BireyselKullanici { get; set; }
+        public KurumsalKullanici KurumsalKullanici { get; set; }
+    }
+}
