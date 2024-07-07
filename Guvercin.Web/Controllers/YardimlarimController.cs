970f1d4b6c79ee21284377ec17804f1a66a4189a
Bilge-Han <bilgeacar095@gmail.com>
2024-07-08 02:34:14 +0300

    AllOkay

--- Guvercin.Web/Controllers/YardimlarimController.cs
+++ Guvercin.Web/Controllers/YardimlarimController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Guvercin.DataAccessLayer;
 using GuvercinApp.Models;
@@ -6,7 +7,6 @@ using GuvercinApp.Models;
 public class YardimlarimController : Controller
 {
     private readonly DataContext _context = new DataContext();
-
     // YardimTaleplerim sayfası
     public ActionResult YardimTaleplerim()
     {
@@ -41,5 +41,130 @@ public class YardimlarimController : Controller
                                  .ToList()
         };
         return View(model);
+    }
+    public ActionResult Islemler()
+    {
+        if (Session["KullaniciId"] == null)
+        {
+            return RedirectToAction("Login", "Acc");
+        }
+
+        int kurumId = (int)Session["KullaniciId"];
+
+        var gelenTalepler = _context.YardimEts
+            .Where(y => y.KurumId == kurumId && y.OnayDurumu == "Beklemede")
+            .ToList();
+
+        var onaylananTalepler = _context.YardimEts
+            .Where(y => y.KurumId == kurumId && y.OnayDurumu == "Onaylandi")
+            .ToList();
+
+        var tamamlananTalepler = _context.YardimEts
+            .Where(y => y.KurumId == kurumId && y.OnayDurumu == "Tamamlandi")
+            .ToList();
+
+        var tamamlanamayanTalepler = _context.YardimEts
+            .Where(y => y.KurumId == kurumId && y.OnayDurumu == "Tamamlanamadi")
+            .ToList();
+
+        var reddedilenTalepler = _context.YardimEts
+            .Where(y => y.KurumId == kurumId && y.OnayDurumu == "Reddedildi")
+            .ToList();
+
+        var butunTalepler = _context.YardimEts.Where(y => y.KurumId == kurumId).ToList();
+
+        var model = new IslemlerViewModel
+        {
+            GelenTalepler = gelenTalepler,
+            OnaylananTalepler = onaylananTalepler,
+            TamamlananTalepler = tamamlananTalepler,
+            TamamlanamayanTalepler = tamamlanamayanTalepler,
+            ReddedilenTalepler = reddedilenTalepler,
+            ButunTalepler = butunTalepler
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Onayla(int id)
+    {
+        var yardimEt = _context.YardimEts.FirstOrDefault(y => y.YardimEtId == id);
+        if (yardimEt != null)
+        {
+            yardimEt.OnayDurumu = "Onaylandi";
+            yardimEt.OnayTarihi = DateTime.Now;
+
+            var yardimTalebi = _context.YardimTalepleri.FirstOrDefault(y => y.YardimTalebiId == yardimEt.YardimTalebiId);
+            if (yardimTalebi != null)
+            {
+                yardimTalebi.Durum = "Onaylandi";
+            }
+
+            _context.SaveChanges();
+        }
+
+        return RedirectToAction("Islemler");
+    }
+
+    [HttpPost]
+    public ActionResult Reddet(int id)
+    {
+        var yardimEt = _context.YardimEts.FirstOrDefault(y => y.YardimEtId == id);
+        if (yardimEt != null)
+        {
+            yardimEt.OnayDurumu = "Reddedildi";
+
+            var yardimTalebi = _context.YardimTalepleri.FirstOrDefault(y => y.YardimTalebiId == yardimEt.YardimTalebiId);
+            if (yardimTalebi != null)
+            {
+                yardimTalebi.Durum = "Reddedildi";
+            }
+
+            _context.SaveChanges();
+        }
+
+        return RedirectToAction("Islemler");
+    }
+
+    [HttpPost]
+    public ActionResult Tamamlandi(int id)
+    {
+        var yardimEt = _context.YardimEts.FirstOrDefault(y => y.YardimEtId == id);
+        if (yardimEt != null)
+        {
+            yardimEt.OnayDurumu = "Tamamlandi";
+            yardimEt.TamamlanmaTarihi = DateTime.Now;
+
+            var yardimTalebi = _context.YardimTalepleri.FirstOrDefault(y => y.YardimTalebiId == yardimEt.YardimTalebiId);
+            if (yardimTalebi != null)
+            {
+                yardimTalebi.Durum = "Tamamlandi";
+            }
+
+            _context.SaveChanges();
+        }
+
+        return RedirectToAction("Islemler");
+    }
+
+    [HttpPost]
+    public ActionResult Tamamlanamadi(int id)
+    {
+        var yardimEt = _context.YardimEts.FirstOrDefault(y => y.YardimEtId == id);
+        if (yardimEt != null)
+        {
+            yardimEt.OnayDurumu = "Tamamlanamadi";
+
+            var yardimTalebi = _context.YardimTalepleri.FirstOrDefault(y => y.YardimTalebiId == yardimEt.YardimTalebiId);
+            if (yardimTalebi != null)
+            {
+                yardimTalebi.Durum = "Tamamlanamadi";
+            }
+
+            _context.SaveChanges();
+        }
+
+        return RedirectToAction("Islemler");
     }
 }
---
+++ Guvercin.Web/Models/IslemlerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuvercinApp.Models
+{
+    public class IslemlerViewModel
+    {
+        public List<YardimEt> GelenTalepler { get; set; }
+        public List<YardimEt> OnaylananTalepler { get; set; }
+        public List<YardimEt> TamamlananTalepler { get; set; }
+        public List<YardimEt> TamamlanamayanTalepler { get; set; }
+        public List<YardimEt> ReddedilenTalepler { get; set; }
+        public List<YardimEt> ButunTalepler { get; set; }
+    }
+}
